ac13d8f73ceab30d17d17d8f2e40e0c3f1ebeac1
Mou Janki <mjanki1990@gmail.com>
2018-02-18 16:28:30 -0600

    Removed redundunt NavigationSing

--- util/src/main/java/org/umbrellahq/util/NavigationUtil.kt
+++ util/src/main/java/org/umbrellahq/util/NavigationUtil.kt
@@ -8,23 +8,21 @@ import android.util.Log
  * Created by mjanki on 2/18/18.
  */
 
-val TAG = "NavigationUtil"
+const val TAG = "NavigationUtil"
 
-class NavigationUtil {
-    companion object NavigationSing {
-        var mainResId: Int = -1
+object NavigationUtil {
+    var mainResId: Int = -1
 
-        fun setup(mainResId: Int) {
-            this.mainResId = mainResId
-        }
+    fun setup(mainResId: Int) {
+        this.mainResId = mainResId
     }
 }
 
 fun FragmentManager.pushFragment(fragment: Fragment) {
-    if (NavigationUtil.NavigationSing.mainResId == -1) {
+    if (NavigationUtil.mainResId == -1) {
         Log.e(TAG, "Setup Main Res Id before using pushFragment")
         return
     }
 
-    beginTransaction().add(NavigationUtil.NavigationSing.mainResId, fragment).commit()
+    beginTransaction().add(NavigationUtil.mainResId, fragment).commit()
 }
