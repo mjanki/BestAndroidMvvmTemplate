c1898d7beca2660230d23d765c891aec67bafd69
Mou Janki <mjanki1990@gmail.com>
2018-02-18 02:53:51 -0600

    MVVM modular structure implemented

--- view/src/main/java/org/umbrellahq/baseapp/ui/fragment/MainFragment.kt
+++ view/src/main/java/org/umbrellahq/baseapp/ui/fragment/MainFragment.kt
@@ -16,8 +16,7 @@ class MainFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        return inflater!!.inflate(R.layout.fragment_main, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
     }
 }
