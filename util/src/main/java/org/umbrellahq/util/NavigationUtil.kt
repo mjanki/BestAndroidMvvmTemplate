8dcc41a0911f436eea37b4204a770900126ca747
Mou Janki <mjanki1990@gmail.com>
2018-02-26 20:54:34 -0600

    Added some comments

--- util/src/main/java/org/umbrellahq/util/NavigationUtil.kt
+++ util/src/main/java/org/umbrellahq/util/NavigationUtil.kt
@@ -32,27 +32,34 @@ fun AppCompatActivity.setupToolbar(toolbar: Toolbar, showUp: Boolean = true, tit
     this.title = title
 }
 
+// Go to new activity
 fun FragmentActivity.pushActivity(cls: KClass<*>, bundle: Bundle? = null, code: Int? = null, fragment: Fragment? = null) {
+    // Create Intent, with extras if available
     val intent = Intent(this, cls.java).apply {
         if (bundle != null) putExtras(bundle)
     }
 
     if (code != null) {
+        // Start activity for result either from Fragment or Activity
         if (fragment != null) startActivityFromFragment(fragment, intent, code)
         else startActivityForResult(intent, code)
     } else startActivity(intent)
 }
 
+// Add ability to call pushActivity from Fragment
 fun Fragment.pushActivity(cls: KClass<*>, bundle: Bundle? = null, code: Int? = null) {
     activity?.pushActivity(cls, bundle, code, this)
 }
 
+// Navigate up
 fun FragmentActivity.popActivity(intent: Intent? = null) {
     if (intent != null) {
+        // Send results back to prior activity
         setResult(Activity.RESULT_OK, intent)
         finish()
     } else {
         try {
+            // Navigate up if no result is expected
             NavUtils.navigateUpFromSameTask(this)
         } catch (e: IllegalArgumentException) {
             Log.e(NavigationUtil.TAG, "Setup android:parentActivityName in Activity's Manifest")
@@ -60,6 +67,7 @@ fun FragmentActivity.popActivity(intent: Intent? = null) {
     }
 }
 
+// Add ability to call popActivity from Fragment
 fun Fragment.popActivity(intent: Intent? = null) {
     activity?.popActivity(intent)
 }
