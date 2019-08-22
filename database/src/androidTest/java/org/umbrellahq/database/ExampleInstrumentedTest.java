186fac6ce85ebde83a2d2224586c8f2b87df17a2
Mou Janki <mjanki1990@gmail.com>
2019-08-22 01:36:31 -0400

    Fixed dependencies for database testing

--- database/src/androidTest/java/org/umbrellahq/database/ExampleInstrumentedTest.java
+++ database/src/androidTest/java/org/umbrellahq/database/ExampleInstrumentedTest.java
@@ -2,13 +2,13 @@ package org.umbrellahq.database;
 
 import android.content.Context;
 
-import androidx.test.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -20,7 +20,7 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = ApplicationProvider.getApplicationContext();
 
         assertEquals("org.umbrellahq.database.test", appContext.getPackageName());
     }
