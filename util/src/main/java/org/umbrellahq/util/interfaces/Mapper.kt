4ce9b152317c7a484c30f194948ac4b0300cffab
Mou Janki <mjanki1990@gmail.com>
2019-11-01 20:33:59 -0400

    Added comments in the Mapper interface

--- util/src/main/java/org/umbrellahq/util/interfaces/Mapper.kt
+++ util/src/main/java/org/umbrellahq/util/interfaces/Mapper.kt
@@ -1,6 +1,9 @@
 package org.umbrellahq.util.interfaces
 
 interface Mapper<T, V> {
+    // Map from current layer entity to specific layer below entity
     fun downstream(currentLayerEntity: T): V
+
+    // Map from specific layer below entity to current layer entity
     fun upstream(nextLayerEntity: V): T
 }
