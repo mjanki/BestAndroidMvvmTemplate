77f5b97234e096a216dcfa9ef75defd1bcca4d79
Mou Janki <mjanki1990@gmail.com>
2020-10-15 02:10:31 -0400

    Added handleNavigationClick to BaseViewModel

--- viewmodel/src/main/java/org/umbrellahq/viewmodel/viewmodels/BaseViewModel.kt
+++ viewmodel/src/main/java/org/umbrellahq/viewmodel/viewmodels/BaseViewModel.kt
@@ -3,9 +3,30 @@ package org.umbrellahq.viewmodel.viewmodels
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import io.reactivex.disposables.CompositeDisposable
+import java.util.*
+import kotlin.concurrent.schedule
 
 open class BaseViewModel(application: Application) : AndroidViewModel(application) {
     protected var disposables = CompositeDisposable()
+    private var clickLock = false
+
+    protected fun handleNavigationClick(delay: Long = 0, function: () -> Unit) {
+        if (!clickLock) {
+            clickLock = true
+
+            Timer().schedule(delay) {
+                function()
+
+                Timer().schedule(100) {
+                    clickLock = false
+                }
+            }
+        }
+    }
+
+    protected fun handleNavigationClickDelayed(function: () -> Unit) {
+        handleNavigationClick(150, function)
+    }
 
     override fun onCleared() {
         disposables.clear()
