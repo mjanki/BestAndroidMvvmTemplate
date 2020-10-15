f2df4df05437921355ad6e6cda3ada33c14ede38
Mou Janki <mjanki1990@gmail.com>
2020-10-15 01:44:17 -0400

    ViewUtil: remove inflate extension and add setVisible extension

--- util/src/main/java/org/umbrellahq/util/ViewUtil.kt
+++ util/src/main/java/org/umbrellahq/util/ViewUtil.kt
@@ -1,10 +1,7 @@
 package org.umbrellahq.util
 
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.annotation.LayoutRes
 
-fun ViewGroup.inflate(@LayoutRes layoutRes: Int, attachToRoot: Boolean = false): View {
-    return LayoutInflater.from(context).inflate(layoutRes, this, attachToRoot)
+fun View.setVisible(visible: Boolean) {
+    visibility = if (visible) { View.VISIBLE } else { View.GONE }
 }
